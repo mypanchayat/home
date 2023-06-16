ec2d82eb336b09276e3ccbc6b8a34f94f909c732
Keshav Mishra <136546552+mypanchayat@users.noreply.github.com>
2023-06-16 10:02:43 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,25 +1,29 @@
-// Get all profile cards
-var cards = document.querySelectorAll('.profile-card');
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-// Add click event listener to each card
-cards.forEach(function(card) {
-  card.addEventListener('click', function() {
-    // Remove active class from all cards
-    cards.forEach(function(c) {
-      c.classList.remove('active');
-    });
-    
-    // Add active class to clicked card
-    this.classList.add('active');
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
   });
-});
-
-// Add click event listener to close icons
-var closeIcons = document.querySelectorAll('.close-icon');
-closeIcons.forEach(function(icon) {
-  icon.addEventListener('click', function(event) {
-    event.stopPropagation();
-    // Remove active class from parent card
-    this.closest('.profile-card').classList.remove('active');
-  });
-});
