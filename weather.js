3cb266d693a5dec0b42300f68a29516ee6cec677
Keshav Mishra <136546552+mypanchayat@users.noreply.github.com>
2023-06-16 18:18:47 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,32 +1,23 @@
-$(document).ready(function () {
-    var pos = 0,
-        slides = $('.slide'),
-        numOfSlides = slides.length;
-
-    function nextSlide() {
-        // `[]` returns a vanilla DOM object from a jQuery object/collection
-        slides[pos].video.stopVideo()
-        slides.eq(pos).animate({ left: '-100%' }, 500);
-        pos = (pos >= numOfSlides - 1 ? 0 : ++pos);
-        slides.eq(pos).css({ left: '100%' }).animate({ left: 0 }, 500);
-    }
-
-    function previousSlide() {
-        slides[pos].video.stopVideo()
-        slides.eq(pos).animate({ left: '100%' }, 500);
-        pos = (pos == 0 ? numOfSlides - 1 : --pos);
-        slides.eq(pos).css({ left: '-100%' }).animate({ left: 0 }, 500);
-    }
-
-    $('.left').click(previousSlide);
-    $('.right').click(nextSlide);
-})
-
-function onYouTubeIframeAPIReady() {
-    $('.slide').each(function (index, slide) {
-        // Get the `.video` element inside each `.slide`
-        var iframe = $(slide).find('.video')[0]
-        // Create a new YT.Player from the iFrame, and store it on the `.slide` DOM object
-        slide.video = new YT.Player(iframe)
-    })
-}
+	   
+	   
+	       var box1 = document.getElementById('box1');
+	       var box2 = document.getElementById('box2');
+	       var box3 = document.getElementById('box3');
+	   
+	       box1.onended = function () {
+	           box2.play();
+	           box1.style.opacity=0;
+	           box2.style.opacity=1;
+	       }
+	       box2.onended = function () {
+	           box3.play();
+	           box2.style.opacity=0;
+	           box3.style.opacity=1;
+	       }
+	       box3.onended = function () {
+	           box1.play();
+	           box3.style.opacity=0;
+	           box1.style.opacity=1;
+	       }
+	   
+	
