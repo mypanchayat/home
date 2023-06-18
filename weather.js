c52875dd1027fd09a1f199b40084506fbc707e11
Keshav Mishra <136546552+mypanchayat@users.noreply.github.com>
2023-06-18 10:02:40 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -8,15 +8,20 @@ wIcon = weatherPart.querySelector("img"),
 arrowBack = wrapper.querySelector("header i");
 
 let api;
+let concatStrings = splitString[0] + splitString[1],
+lastString = ["c", "7", "e", "e"],
+reverseString = lastString.reverse().join("");
 
 inputField.addEventListener("keyup", e =>{
+    // if user pressed enter btn and input value is not empty
     if(e.key == "Enter" && inputField.value != ""){
         requestApi(inputField.value);
+        inputField.blur();
     }
 });
 
 locationBtn.addEventListener("click", () =>{
-    if(navigator.geolocation){
+    if(navigator.geolocation){ // if browser support geolocation api
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }else{
         alert("Your browser not support geolocation api");
@@ -24,40 +29,53 @@ locationBtn.addEventListener("click", () =>{
 });
 
 function requestApi(city){
-    api = "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=8b03c17b89f8a35d882dc4245d9b33d9";
+    const key = reverseString + concatStrings;
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
     fetchData();
 }
 
 function onSuccess(position){
-    const {latitude, longitude} = position.coords;
-    api = "https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=8b03c17b89f8a35d882dc4245d9b33d9";
+    const {latitude, longitude} = position.coords; // getting lat and lon of the user device from coords obj
+    const key = reverseString + concatStrings;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${key}`;
     fetchData();
 }
 
 function onError(error){
-    infoTxt.innerText = error.message;
     infoTxt.classList.add("error");
+    // if any error occur while getting user location then we'll show it in infoText
+    infoTxt.innerText = error.message;
 }
 
 function fetchData(){
     infoTxt.innerText = "Getting weather details...";
     infoTxt.classList.add("pending");
+    // getting api response and returning it with parsing into js obj and in another 
+    // then function calling weatherDetails function with passing api result as an argument
     fetch(api).then(res => res.json()).then(result => weatherDetails(result)).catch(() =>{
-        infoTxt.innerText = "Something went wrong";
         infoTxt.classList.replace("pending", "error");
+        infoTxt.innerText = "Something went wrong";
     });
 }
 
 function weatherDetails(info){
-    if(info.cod == "404"){
+    if(info.cod == "404"){ // if user entered city name isn't valid
         infoTxt.classList.replace("pending", "error");
         infoTxt.innerText = `${inputField.value} isn't a valid city name`;
     }else{
+        //getting required properties value from the whole weather information
         const city = info.name;
-        const country = info.sys.country;
+        let country = info.sys.country;
         const {description, id} = info.weather[0];
         const {temp, feels_like, humidity} = info.main;
 
+        for(i in countryList){
+            if(i == country){
+              country = countryList[i];
+            }
+          }
+        
+        // custom weather icon according to the id which api gives to us
         if(id == 800){
             wIcon.src = "icons/clear.svg";
         }else if(id >= 200 && id <= 232){
@@ -72,15 +90,18 @@ function weatherDetails(info){
             wIcon.src = "icons/rain.svg";
         }
         
+        //passing a particular weather info to a particular element
         weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
         weatherPart.querySelector(".weather").innerText = description;
         weatherPart.querySelector(".location span").innerText = `${city}, ${country}`;
         weatherPart.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
         weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
-        infoTxt.classList.remove("pending", "error");
-        infoTxt.innerText = "";
-        inputField.value = "";
-        wrapper.classList.add("active");
+        setTimeout(()=>{
+            infoTxt.classList.remove("pending", "error");
+            infoTxt.innerText = "";
+            inputField.value = "";
+            wrapper.classList.add("active");
+        }, 800);
     }
 }
 
